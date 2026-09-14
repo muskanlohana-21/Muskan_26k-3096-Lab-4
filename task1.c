029e04a2eace770b87bb7f773dbe579e06e9781d
muskanlohana-21 <muskanlahana@gmail.com>
2026-09-14 12:36:56 -0700

    add post lab task1.c
    Fix input and output function names, and correct condition check for income.

---
+++ task1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+  int marks;
+  float income;
+
+  printf("enter marks");
+  scanf("%d", &marks);
+  printf("enter family income");
+  sacnf("%f", &income);
+  if (marks>=80 || INCOME <50000)
+  {
+    PRINTF("Student qualifies for scholarship");
+  }
+  else
+  {
+    printf("student does not qualifies for scholarship");
+  }
+}
