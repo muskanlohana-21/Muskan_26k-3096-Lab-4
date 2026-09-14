574a02f7b697431a934f76de1a9350102c593066
muskanlohana-21 <muskanlahana@gmail.com>
2026-09-14 12:01:07 -0700

    add in lab tasl1.c
    Fix variable name typo and correct print function.

---
+++ tasl1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  int num1, num2, sum;
+  printf("enter num1");
+  scanf("%d", &num1);
+
+  printf("enter num2");
+  scanf("%d", &num2);
+
+  sum = mum1 + num2
+    prinf("sum =%d\n", sum);
+}
