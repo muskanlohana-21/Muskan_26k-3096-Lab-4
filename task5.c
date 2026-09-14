74e88903c9b5299f000a7c38c59169a0ab8965be
muskanlohana-21 <muskanlahana@gmail.com>
2026-09-14 12:20:06 -0700

    add in lab task5.c
    Fix variable name and typos in grade output.

---
+++ task5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+  int marks;
+
+  printf("enter students marks");
+  scanf("%d", &msrks);
+  if (marks >=80)
+    printf("grade A\n");
+  else if (marks>=60)
+    printf("gtade B\n");
+      else
+    printf("grade C\n");
+}
