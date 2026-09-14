842588ac80ef7b2a170366614234f9dd24c7ae4c
muskanlohana-21 <muskanlahana@gmail.com>
2026-09-14 12:07:33 -0700

    add in lab task2.c

---
+++ task2.c
@@ -0,0 +1,8 @@
+#include <stdio>
+int main() {
+  int totalmarks =100;
+  float obtainmarks =90;
+  float percentage =obtainmarks/totalmarks*100;
+  printf("your obtainmarks %f", obtainmarks);
+    printf("your percentage %f", percentage);
+}
