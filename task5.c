d03dce83a0613a6c73c9fcb22129aea6e9848503
muskanlohana-21 <muskanlahana@gmail.com>
2026-09-14 22:08:56 -0700

    add psot lab task5.c

---
+++ task5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+  int num, square, cude;
+  printf("enter an integer");
+  scanf("%d", &num);
+  square = num*num;
+  cude = num*num*num;
+  printf("square = %d"\n, square);
+  printf("cude = %d\n", cude);
+}
